7016912dbd76de567e825539b97a8394ff4a2445
Makom64 <yellowffd400@gmail.com>
2026-09-03 16:28:20 +0900

    [Feat] 좌우 방향으로 서서히 빨라지는 회전 구현

--- Unity_Helicopter/Assets/Scripts/HeliController.cs
+++ Unity_Helicopter/Assets/Scripts/HeliController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class HeliController : MonoBehaviour
 {
     [SerializeField] private float _coreSpeed;
-
+    private const string HORIZON = "Horizontal";
+    private float _coreTurn;
     private void Update()
     {
         CoreMove();
@@ -26,6 +27,28 @@ public class HeliController : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * _coreSpeed * Time.deltaTime);
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            _coreTurn += Input.GetAxis(HORIZON) * _coreSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up * _coreTurn *  _coreSpeed * 5 * Time.deltaTime);
+        }
+        
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            _coreTurn = 0;
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            _coreTurn += Input.GetAxis(HORIZON) * _coreSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up *  _coreTurn * _coreSpeed * 5 * Time.deltaTime);
+        }
+        
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            _coreTurn = 0;
         }
     }
 }
